99c891492342c5121ff777b021cf4b4f1996ddde
antony waweru <anto.waweru2030@gmail.com>
2023-01-09 05:59:22 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code your solution here
+//   code your solution here        
+function saturdayFun(target="roller-skate"){
+    console.log(`This Saturday, I want to ${target}`)
+}
+
+
+saturdayFun()
+
+function mondayWork(space="go to the airport"){
+    console.log(`This Monday I will ${space}`);
+}
+mondayWork("go to the office")
+
+
+
+function outerScope(greeting, msg="it's a good day to be good"){
+    const innerScope = function(name, lang = "python"){
+     return `$(greeting), $(name)! $(msg) $(lang)`;
+    }
+
+    return innerScope("student","JS");
+}
+
+outerScope();
+
+
+function wrapAdjective(name, adjective="special"){
+    return `You are $(adjective)`
+
+}
+
+wrapAdjective("%")("a dedicated programmer")
+
